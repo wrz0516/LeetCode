7d55ad4d57437e7082df769cbe16c1f1e33aaae9
wrz <799592560@qq.com>
2023-03-11 14:39:33 +0800

    链表合并

---
+++ src/lianbiao/ListMerge.java
@@ -0,0 +1,20 @@
+package lianbiao;
+
+public class ListMerge {
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int val) { this.val = val; next=null; }
+    }
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode preA=headA;
+        ListNode preB=headB;
+        while(preA!=null){
+            while(preB.next!=null){
+
+            }
+        }
+        return null;
+
+    }
+}
